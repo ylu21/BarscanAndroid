47285b1b814b32a9cfbf557a0b4d7f134847dddb
ylu21 <ylu21@u.rochester.edu>
2015-11-22 03:48:40 -0500

    Added database listener

--- app/src/main/java/com/ylu21/barcodescan/MainActivity.java
+++ app/src/main/java/com/ylu21/barcodescan/MainActivity.java
@@ -15,6 +15,16 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     private static String mContent="";
     private TextView mTv;
@@ -26,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
         if(savedInstanceState!=null){
             mContent = savedInstanceState.getString("KEY_STRING");
             mTv.setText("Code: " + mContent);
+            Thread thread = new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        //Your code goes here
+                        posting(mContent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
         }
 
     }
@@ -48,23 +71,37 @@ public class MainActivity extends AppCompatActivity {
         if(result!=null){
             mTv.setText("Code: " + result.getContents());
             mContent = result.getContents();
-
         }
     }
 
-    public void downloadFromMarket() {
-        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(this);
-        downloadDialog.setTitle("Warning");
-        downloadDialog.setMessage("Barcode app not found. Download?");
-        downloadDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Uri uri = Uri.parse("market://search?q=pname:com.google.zxing.client.android");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+    public void posting(String content){
+        String url = "http://urcsc170.org/ylu21/webplatform/androidphp/result.php?result="+ content;
+        BufferedReader in = null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            int rescode = connection.getResponseCode();
 
-        downloadDialog.show();
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputlLine;
+            StringBuffer response = new StringBuffer();
+            while((inputlLine = in.readLine())!=null){
+                response.append(inputlLine);
+            }
+            Log.d("TAG","find a seat"+response);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(in != null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     @Override
